6be9852313592cbb5d861a72812219b2c538edfb
Lucas Veljacic <lucas.veljacic@gmail.com>
2020-06-13 14:02:37 -0300

    deleting comment

--- CODIGO/app/activity_recognition/app/src/main/java/org/activityrecognition/external/client/model/ModelClientFactory.java
+++ CODIGO/app/activity_recognition/app/src/main/java/org/activityrecognition/external/client/model/ModelClientFactory.java
@@ -5,7 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ModelClientFactory {
     private static ModelClient client;
     private static final String BASE_URL = "http://18.221.185.88:8080";
-    //private static final String BASE_URL = "http://192.168.0.8:8080";
 
     public static ModelClient getClient() {
         if (client == null) {
