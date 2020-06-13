ec355cfa4f76eff09491c703b28ce60fb0ac543f
Lucas Veljacic <lucas.veljacic@gmail.com>
2020-06-13 02:00:50 -0300

    minor change

--- CODIGO/app/activity_recognition/app/src/main/java/org/activityrecognition/external/client/auth/AuthClient.java
+++ CODIGO/app/activity_recognition/app/src/main/java/org/activityrecognition/external/client/auth/AuthClient.java
@@ -4,7 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
-
 public interface AuthClient {
         @POST("/api/api/register")
         Call<AuthResponse> signUp(@Body SignUpDTO userSignUpDTO);
